e567e1ef9d7f5628fd48bf421146bc3cdfbb3b98
Ricky Sheaves <r@rocketsquawk.com>
2013-08-27 08:51:02 -0400

    Refactor builds#show to render dependants and add more seed data

--- app/models/build.rb
+++ app/models/build.rb
@@ -3,4 +3,8 @@ class Build < ActiveRecord::Base
   has_many :dependencies
   has_many :dependants, :class_name => 'Dependency', :foreign_key => 'dependency_build_id'
   attr_accessible :project_id, :build_number
+
+  def project_name
+  	return Project.find(self.project_id).name
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Project.create(name: 'Foo', description: 'The classic foo project, which will have a dependency on Bar.')
 Project.create(name: 'Bar', description: 'The classic bar project, which will be a dependant of Foo.')
+Project.create(name: 'NaviNetEndUserKeyLogger', description: 'Top secret. Move along. Nothing to see here.')
 
 # A series of Foo builds that depend on Bar-2.0.0.1
 # This is a typical dev cycle
@@ -22,3 +23,7 @@ Dependency.create(build_id: 4, dependency_build_id: 1)
 Build.create(project_id: 2, build_number: '2.1.0.1') #5
 Build.create(project_id: 1, build_number: '1.1.0.1') #6
 Dependency.create(build_id: 6, dependency_build_id: 5)
+
+Build.create(project_id: 3, build_number: '6.9.0.1') #7
+Dependency.create(build_id: 7, dependency_build_id: 5)
+Dependency.create(build_id: 7, dependency_build_id: 6)
